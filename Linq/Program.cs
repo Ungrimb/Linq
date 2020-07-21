a72cb5187e45a21dd08476823b2b54bde5d5b3af
Ismael <arkarash@gmail.com>
2020-07-21 18:59:15 +0200

    Average,Min,Max

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -8,12 +8,13 @@ namespace Linq
         static void Main(string[] args)
         {
             int[] numbers = new int[] { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 };
-            var pares = numbers.Where(x => x % 2 == 0).ToList();
+            var media = numbers.Average(x => x);
+            var max = numbers.Max(x => x);
+            var min = numbers.Min(x => x);
 
-            foreach (var n in pares)
-            {
-                Console.WriteLine(n);
-            }
+            Console.WriteLine("La media de los números es {0}\nCon un máximo de {1}\nY un mínimo de {2}",media,max,min);
+
+     
         }
     }
 }
