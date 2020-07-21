2b472f8b95aa6fa2461f0aed8ac21ef1d2b19ac4
Ismael <arkarash@gmail.com>
2020-07-21 18:49:45 +0200

    FASE 1

---
+++ Linq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 };
+            var pares = numbers.Where(x => x % 2 == 0).ToList();
+
+            foreach (var n in pares)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
